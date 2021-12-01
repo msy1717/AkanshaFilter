737829057a10821d760092455bcf77a6890caf9d
Anshvachhani99 <92299607+Anshvachhani99@users.noreply.github.com>
2021-12-01 10:26:54 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -12,10 +12,10 @@ class script(object):
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
 - Eva Maria is a open source project. 
-- Source - https://github.com/EvamariaTG/EvaMaria  
+- Source - https://github.com/Anshvachhani99/EvaMaria/edit/master/Script.py  
 
 <b>DEVS:</b>
-- <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>"""
+- <a href=https://github.com/Anshvachhani99/EvaMaria/edit/master/Script.py>Team Eva Maria</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
