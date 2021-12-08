0c018181444c111009b8e240dc32d709bbf8d2fe
Anshvachhani99 <92299607+Anshvachhani99@users.noreply.github.com>
2021-12-08 18:14:52 +0530

    Update pm_filter(1).py

--- plugins/pm_filter(1).py
+++ plugins/pm_filter(1).py
@@ -669,7 +669,7 @@ async def auto_filter(client, msg, spoll=False):
             await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
     else:
         r = await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
-        await asyncio.sleep(400)
+        await asyncio.sleep(1800)
         await r.delete()
         return
 
