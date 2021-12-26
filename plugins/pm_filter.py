5e23333a12b89a6fd2988162bf1866592ce146ab
Anshvachhani99 <92299607+Anshvachhani99@users.noreply.github.com>
2021-12-26 11:19:41 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -99,7 +99,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"📂[{get_size(file.file_size)}] 🎥 {file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"💝 [{get_size(file.file_size)}] 💖 {file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
@@ -108,7 +108,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"📂[{get_size(file.file_size)}] 🎥 {file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"💖 [{get_size(file.file_size)}] ❤️‍🔥 {file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
