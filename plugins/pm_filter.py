a4384fb9b7859129d6d825ce2a126466bcba96e5
Anshvachhani99 <92299607+Anshvachhani99@users.noreply.github.com>
2022-01-23 13:05:18 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -25,7 +25,6 @@ SPELL_CHECK = {}
 
 @Client.on_message(filters.group & filters.text & filters.incoming)
 async def give_filter(client,message):
-    await message.delete(300)
     group_id = message.chat.id
     name = message.text
 
